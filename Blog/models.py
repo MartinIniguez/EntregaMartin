995c5f89d375a450c470d76433eef0bc5aa08ebb
Your Name <you@example.com>
2025-06-16 19:16:19 -0300

    borre algo

--- Blog/models.py
+++ Blog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
     contenido = models.TextField()
     fecha_publicacion = models.DateTimeField(auto_now_add=True)
     autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
-    categorias = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='posts')
+    categorias = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.titulo
